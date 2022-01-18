343a4952a18207a42268a1090de5e3c7e97c7d1c
dnyandip18 <96012124+dnyandip18@users.noreply.github.com>
2022-01-18 16:00:13 +0530

    Delete pra13.py

--- pra13.py
+++
@@ -1,7 +0,0 @@
-#write a python program to find sum of natural number
-start=int(input("enter start number:"))
-end=int(input("enter end number"))
-s=0
-for i in range(start,end+1):
-    s=s+i
-print("sum of natural number==",s)
