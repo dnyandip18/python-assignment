3ea568d9b999470d912b7360f15197472550be91
dnyandip18 <96012124+dnyandip18@users.noreply.github.com>
2022-01-18 16:01:19 +0530

    Delete pra4.py

--- pra4.py
+++
@@ -1,8 +0,0 @@
-#python program to check if a number is positive negative or zero
-num=int(input("enter the number"))
-if(num==0):
-    print("number is zero")
-elif(num>0):
-    print("number is positive")
-else:
-        print("number is negative")
