7f411315b9c1ca8fcfed78ad73fb2321f16a19f9
dnyandip18 <96012124+dnyandip18@users.noreply.github.com>
2022-01-18 16:00:54 +0530

    Delete pra2.py

--- pra2.py
+++
@@ -1,8 +0,0 @@
-#python program to swap two variable
-a=input("enter variable first:")
-b=input("enter variable second")
-t=a
-a=b
-b=t
-print("after swapping variable first:",a)
-print("after swapping varriable second:",b)
